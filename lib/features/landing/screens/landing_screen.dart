74fd813dfafe419f0667627f7e37689465b02890
saritaY41195 <y.sarita41195@gmail.com>
2023-01-28 18:06:04 +0530

    done

---
+++ lib/features/landing/screens/landing_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class LandingScreen extends StatelessWidget {
+  const LandingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          // ignore: prefer_const_literals_to_create_immutables
+          children: [
+            const SizedBox(height: 50),
+            const Text(
+              'Welcome to WhatsApp',
+              style: TextStyle(
+                fontSize: 33,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(height: size.height / 9),
+            Image.asset('assets/bg.jpeg', height: 340, width: 340),
+          ],
+        ),
+      ),
+    );
+  }
+}
